9bbaf452f5aae1df27e6aafd303c28215f75d45d
Ted M. Young <tedyoung@gmail.com>
2023-04-06 11:47:04 -0700

    LAB #4: Fixed FEATURE ENVY for determining card color from the Suit

--- src/main/java/com/jitterted/ebp/blackjack/Card.java
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -33,7 +33,8 @@ public class Card {
         lines[5] = String.format("│       %s%s│", rank.equals("10") ? "" : " ", rank);
         lines[6] = "└─────────┘";
 
-        Ansi.Color cardColor = "♥♦".contains(suit.symbol()) ? Ansi.Color.RED : Ansi.Color.BLACK;
+        // MAPPING a DOMAIN concept into DISPLAY code
+        Ansi.Color cardColor = suit.isRed() ? Ansi.Color.RED : Ansi.Color.BLACK;
         return ansi()
                 .fg(cardColor).toString()
                 + String.join(ansi().cursorDown(1)

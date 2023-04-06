dac6792e90502a77273cd64f4da0368908023df9
Ted M. Young <tedyoung@gmail.com>
2023-04-06 11:14:46 -0700

    LAB #4: Completed: using only new Card constructor

--- src/main/java/com/jitterted/ebp/blackjack/Suit.java
+++ src/main/java/com/jitterted/ebp/blackjack/Suit.java
@@ -7,6 +7,8 @@ public enum Suit {
     DIAMONDS("♦"),
     HEARTS("♥"),
     CLUBS("♣");
+
+    @Deprecated
     static final List<String> SUITS = List.of(SPADES.symbol,
                                               DIAMONDS.symbol,
                                               HEARTS.symbol,
